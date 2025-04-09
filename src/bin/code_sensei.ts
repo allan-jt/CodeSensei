1ff0a4866c1f6be977d10910ed50e654bb6d4a0c
allan-jt <allan.thekkepeedika@gmail.com>
2025-04-09 17:16:25 -0400

    MongoDb data scrips uploaded

--- src/bin/code_sensei.ts
+++ src/bin/code_sensei.ts
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { CodeSenseiStack } from "../lib/code_sensei-stack";
+import { DynamoStack } from "../lib/storage/dynamo-stack";
+import * as dotenv from "dotenv";
+import * as path from "path";
+
+dotenv.config({ path: path.join(__dirname, "../.env") });
 
 const app = new cdk.App();
-new CodeSenseiStack(app, "CodeSenseiStack", {});
+cdk.Tags.of(app).add("awsApplication", process.env.TAG_VALUE || "undefined");
+
+new CodeSenseiStack(app, "CodeSenseiStack", {}); // For test purposes; remove later
+new DynamoStack(app, "DynamoStack", {});
