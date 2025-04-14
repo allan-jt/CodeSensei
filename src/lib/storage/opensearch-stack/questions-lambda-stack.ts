005e7bf890e45769f61b31ce798a964e61745210
allan-jt <allan.thekkepeedika@gmail.com>
2025-04-14 14:49:04 -0400

    Completed aws opensearch and lambda

--- src/lib/storage/opensearch-stack/questions-lambda-stack.ts
+++ src/lib/storage/opensearch-stack/questions-lambda-stack.ts
@@ -1,5 +1,6 @@
 import { PythonFunction } from "@aws-cdk/aws-lambda-python-alpha";
 import * as cdk from "aws-cdk-lib";
+import { TableV2 } from "aws-cdk-lib/aws-dynamodb";
 import { Construct } from "constructs";
 
 interface LambdaStackProps extends cdk.StackProps {
@@ -7,13 +8,16 @@ interface LambdaStackProps extends cdk.StackProps {
   collectionName: string;
   role: cdk.aws_iam.Role;
   region: string;
+  dynamoTable: TableV2;
 }
 
-export class LambdaForOpenSearchStack extends cdk.Stack {
+export class QuestionsLambdaStack extends cdk.Stack {
+  public readonly fn: PythonFunction;
+
   constructor(scope: Construct, id: string, props: LambdaStackProps) {
     super(scope, id, props);
 
-    const fn = new PythonFunction(this, id, {
+    this.fn = new PythonFunction(this, id, {
       entry: __dirname + "/lambda-code",
       runtime: cdk.aws_lambda.Runtime.PYTHON_3_13,
       handler: "handler",
@@ -22,8 +26,12 @@ export class LambdaForOpenSearchStack extends cdk.Stack {
         OPENSEARCH_COLLECTION: props.collectionName,
         OPENSEARCH_ENDPOINT: props.endpoint,
         AWSREGION: props.region,
+        DYNAMO_TABLE_NAME: props.dynamoTable.tableName,
       },
       role: props.role,
+      timeout: cdk.Duration.seconds(60),
     });
+
+    props.dynamoTable.grantReadWriteData(this.fn);
   }
 }
--- src/lib/storage/storage-stack.ts
+++ src/lib/storage/storage-stack.ts
@@ -2,14 +2,16 @@ import * as cdk from "aws-cdk-lib";
 import { DynamoStack } from "./dynamo-stack/dynamo-stack";
 import { Construct } from "constructs";
 import { OpenSearchStack } from "./opensearch-stack/opensearch-stack";
-import { OpenSearchLambdaStack } from "./opensearch-stack/opensearch";
 
 export class StorageStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // new DynamoStack(this, "DynamoStack");
-    new OpenSearchStack(this, "OpenSearchStack");
-    // new OpenSearchLambdaStack(this, "OpenSearchLambdaStack");
+    const dynamo = new DynamoStack(this, "DynamoStack");
+
+    // Comment out if you don't want to create OpenSearch
+    new OpenSearchStack(this, "OpenSearchStack", {
+      dynamoTable: dynamo.questionBankTable,
+    });
   }
 }
