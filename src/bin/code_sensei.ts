abf7451f3dab0350bef530bdf4c6199065c83f33
allan-jt <allan.thekkepeedika@gmail.com>
2025-04-18 09:56:47 -0400

    Converted opensearch and lambda stacks to constructs; need to rewrite lambda code

--- src/bin/code_sensei.ts
+++ src/bin/code_sensei.ts
@@ -3,10 +3,11 @@ import * as cdk from "aws-cdk-lib";
 import * as dotenv from "dotenv";
 import * as path from "path";
 import { StorageStack } from "../lib/storage/storage-stack";
+import { CodeSenseiStack } from "../lib/code_sensei-stack";
 
 dotenv.config({ path: path.join(__dirname, "../.env") });
 
 const app = new cdk.App();
 cdk.Tags.of(app).add("awsApplication", process.env.TAG_VALUE || "undefined");
 
-new StorageStack(app, "StorageStack");
+new CodeSenseiStack(app, "CodeSenseiStack");
