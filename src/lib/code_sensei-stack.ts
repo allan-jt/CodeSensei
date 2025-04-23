f54d34eb353c5c013e9773aac2557627426ef176
allan-jt <allan.thekkepeedika@gmail.com>
2025-04-23 10:50:21 -0400

    Reorganized directory structure; set up sqs queue with producer and consumer lambdas

--- src/lib/code_sensei-stack.ts
+++ src/lib/code_sensei-stack.ts
@@ -1,11 +1,13 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { StorageStack } from "./storage/storage-stack";
+import { ExecutorStack } from "./executor/executor-stack";
 
 export class CodeSenseiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const storage = new StorageStack(this, "StorageStack");
+    // const storage = new StorageStack(this, "StorageStack");
+    const executor = new ExecutorStack(this, "ExecutorStack");
   }
 }
---
+++ src/lib/executor/queue-stack/queue-stack.ts
@@ -0,0 +1,41 @@
+import * as cdk from "aws-cdk-lib";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
+import { Queue } from "aws-cdk-lib/aws-sqs";
+import { Construct } from "constructs";
+import path = require("path");
+
+export class QueueStack extends cdk.Stack {
+  public readonly producerLambda: Function;
+  public readonly consumerLambda: Function;
+  public readonly sqsQueue: Queue;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.sqsQueue = new Queue(this, "ExecutorSQSQueue");
+
+    this.producerLambda = new Function(this, "ExecutorProducerLambda", {
+      runtime: Runtime.PYTHON_3_13,
+      code: Code.fromAsset(path.join(__dirname, "lambda-code/producer")),
+      handler: "index.handler",
+      environment: {
+        QUEUE_URL: this.sqsQueue.queueUrl,
+      },
+      timeout: cdk.Duration.seconds(10),
+      functionName: "ExecutorProducerLambda",
+    });
+
+    this.consumerLambda = new Function(this, "ExecutorConsumerLambda", {
+      runtime: Runtime.PYTHON_3_13,
+      code: Code.fromAsset(path.join(__dirname, "lambda-code/consumer")),
+      handler: "index.handler",
+      timeout: cdk.Duration.seconds(10),
+      functionName: "ExecutorConsumerLambda",
+    });
+
+    this.sqsQueue.grantSendMessages(this.producerLambda);
+    this.sqsQueue.grantConsumeMessages(this.consumerLambda);
+    this.consumerLambda.addEventSource(new SqsEventSource(this.sqsQueue));
+  }
+}
