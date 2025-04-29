711692f350dc613d870444ed4e01365a527eabae
allan-jt <allan.thekkepeedika@gmail.com>
2025-04-29 12:27:58 -0400

    ECS service set up; need to add docker image and conenct to consumer queue lambda

--- src/lib/executor/executor-stack.ts
+++ src/lib/executor/executor-stack.ts
@@ -1,11 +1,13 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { QueueStack } from "./queue-stack/queue-stack";
+import { CodeExecStack } from "./code-exec-stack/code-exec-stack";
 
 export class ExecutorStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queueStack = new QueueStack(this, "ExecutorQueueStack");
+    // const queueStack = new QueueStack(this, "ExecutorQueueStack");
+    const executorStack = new CodeExecStack(this, "ExecutorCodeExecStack");
   }
 }
