48166863fd46e6e0e347284d14372977c6be3d08
Aaron Bengochea <71156554+aaronbengochea@users.noreply.github.com>
2025-04-30 02:07:59 -0400

    adding AssessmentStack to the code_sensei-stack

--- src/lib/code_sensei-stack.ts
+++ src/lib/code_sensei-stack.ts
@@ -1,11 +1,18 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { StorageStack } from "./storage/storage-stack";
+import { AssessmentStack } from './assessment/assessment-stack';
+
 
 export class CodeSenseiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const storage = new StorageStack(this, "StorageStack");
+
+    const assessment = new AssessmentStack(this, "AssessmentStack",{
+      env: props?.env,
+    });
   }
 }
+
