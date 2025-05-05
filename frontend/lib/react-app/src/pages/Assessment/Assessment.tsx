7074a8397eaae8108ab75ab0f968a3929ab07e3a
allan.thekkepeedika <allan.thekkepeedika@gmail.com>
2025-05-05 21:42:41 +0000

    Question card done; aligned code editor design-wise using card component

--- frontend/lib/react-app/src/pages/Assessment/Assessment.tsx
+++ frontend/lib/react-app/src/pages/Assessment/Assessment.tsx
@@ -1,33 +1,43 @@
 import { useState } from "react";
 import CodeEditorComponent from "./components/CodeEditor";
+import { Stack } from "@mantine/core";
+import QuestionInfoComponent from "./components/QuestionInfo";
 
 function AssessmentPage() {
-    const languages = ["python", "javascript"]
-    const languageSnippets = [
-        "def solution(nums, target):\n    # Your code here\n    pass",
-        "function solution(nums, target) {\n  // Your code here\n}",
-    ]
+  const languages = ["python", "javascript"];
+  const languageSnippets = [
+    "def solution(nums, target):\n    # Your code here\n    pass",
+    "function solution(nums, target) {\n  // Your code here\n}",
+  ];
 
-    const [language, setLanguage] = useState(languages[0]);
-    const [code, setCode] = useState(languageSnippets[0]);
-    const [codeOutput, setCodeOutput] = useState("Output will appear hdere")
+  const [language, setLanguage] = useState(languages[0]);
+  const [code, setCode] = useState(languageSnippets[0]);
+  const [codeOutput, setCodeOutput] = useState("Output will appear hdere");
 
-    const handleSubmit = () => {
-        console.log(language);
-        console.log(code);
-        setCodeOutput(`Used ${language}:\n${code}`)
-    }
+  const handleSubmit = () => {
+    console.log(language);
+    console.log(code);
+    setCodeOutput(`Used ${language}:\n${code}`);
+  };
 
-    return (
-        <CodeEditorComponent
-            languages={languages}
-            languageSnippets={languageSnippets}
-            codeOutput={codeOutput}
-            getLanguage={setLanguage}
-            getCode={setCode}
-            handleSubmit={handleSubmit}
-        />
-    );
+  return (
+    <Stack align="stretch" justify="center" gap="md">
+      <QuestionInfoComponent
+        title="Two Sum"
+        description="Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target."
+        topic={["Array", "Two Pointers"]}
+        difficulty="easy"
+      />
+      <CodeEditorComponent
+        languages={languages}
+        languageSnippets={languageSnippets}
+        codeOutput={codeOutput}
+        getLanguage={setLanguage}
+        getCode={setCode}
+        handleSubmit={handleSubmit}
+      />
+    </Stack>
+  );
 }
 
 export default AssessmentPage;
