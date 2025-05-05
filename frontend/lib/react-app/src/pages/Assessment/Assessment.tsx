2eebe1573914ff8119f756bb88d7996afab58eea
allan.thekkepeedika <allan.thekkepeedika@gmail.com>
2025-05-05 19:24:42 +0000

    Frontend code editor component done

--- frontend/lib/react-app/src/pages/Assessment/Assessment.tsx
+++ frontend/lib/react-app/src/pages/Assessment/Assessment.tsx
@@ -1,7 +1,33 @@
-import { Title } from "@mantine/core";
+import { useState } from "react";
+import CodeEditorComponent from "./components/CodeEditor";
 
 function AssessmentPage() {
-    return <Title>"Assessment page"</Title>
+    const languages = ["python", "javascript"]
+    const languageSnippets = [
+        "def solution(nums, target):\n    # Your code here\n    pass",
+        "function solution(nums, target) {\n  // Your code here\n}",
+    ]
+
+    const [language, setLanguage] = useState(languages[0]);
+    const [code, setCode] = useState(languageSnippets[0]);
+    const [codeOutput, setCodeOutput] = useState("Output will appear hdere")
+
+    const handleSubmit = () => {
+        console.log(language);
+        console.log(code);
+        setCodeOutput(`Used ${language}:\n${code}`)
+    }
+
+    return (
+        <CodeEditorComponent
+            languages={languages}
+            languageSnippets={languageSnippets}
+            codeOutput={codeOutput}
+            getLanguage={setLanguage}
+            getCode={setCode}
+            handleSubmit={handleSubmit}
+        />
+    );
 }
 
 export default AssessmentPage;
