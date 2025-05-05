7120d7e5df092be9716d29c2c684267a500cd420
allan.thekkepeedika <allan.thekkepeedika@gmail.com>
2025-05-05 19:24:42 +0000

    Frontend code editor component done

--- frontend/lib/react-app/src/main.tsx
+++ frontend/lib/react-app/src/main.tsx
@@ -2,11 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MantineProvider>
+    <ColorSchemeScript forceColorScheme="dark" />
+    <MantineProvider forceColorScheme="dark">
       <App />
     </MantineProvider>
   </StrictMode>
---
+++ frontend/lib/react-app/src/pages/Assessment/components/CodeEditor.tsx
@@ -0,0 +1,85 @@
+import { Box, Button, Group, Paper, ScrollArea, SegmentedControl, Stack, Text } from "@mantine/core";
+import Editor from "@monaco-editor/react";
+import { useState } from "react";
+
+interface CodeEditorProps {
+  languages: string[];
+  languageSnippets: string[];
+  codeOutput: string;
+  getLanguage: (code: any) => void;
+  getCode: (language: any) => void;
+  handleSubmit: () => void;
+}
+
+function CodeEditorComponent({
+  languages,
+  languageSnippets,
+  codeOutput,
+  getLanguage,
+  getCode,
+  handleSubmit
+}:
+  CodeEditorProps) {
+  const [language, setLanguage] = useState(languages[0]);
+  const [code, setCode] = useState(languageSnippets[0]);
+
+  const handleLanguageChange = (selectedLanguage: string) => {
+    const index = languages.indexOf(selectedLanguage);
+    const newCode = languageSnippets[index] || "";
+    setLanguage(selectedLanguage);
+    setCode(newCode);
+
+    getLanguage(selectedLanguage)
+    getCode(newCode)
+  };
+
+  const handleEditorChange = (newCode: string | undefined) => {
+    const updatedCode = newCode || "";
+    setCode(updatedCode);
+    getLanguage(language)
+    getCode(newCode)
+  };
+
+  return (
+    <Paper shadow="md" radius="md" p="xl" withBorder>
+      <Stack align="flex-start" bg="var(--mantine-color-body)">
+
+        <Group justify="space-between" w="100%" mb="md">
+          <SegmentedControl
+            value={language}
+            onChange={handleLanguageChange}
+            data={languages}
+          />
+          <Button onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Group>
+
+        <Editor
+          height="50vh"
+          language={language}
+          value={code}
+          onChange={handleEditorChange}
+          theme="vs-dark"
+        />
+
+        <ScrollArea
+          h={250}
+          w="100%"
+          bg="#1e1e1e"
+          c="#d4d4d4"
+          ff="monospace"
+          p="md"
+          offsetScrollbars
+          scrollHideDelay={500}>
+          <Text size="sm" style={{ whiteSpace: "pre-wrap" }}>
+            {codeOutput}
+          </Text>
+        </ScrollArea>
+
+      </Stack>
+    </Paper >
+  );
+}
+
+export default CodeEditorComponent;
