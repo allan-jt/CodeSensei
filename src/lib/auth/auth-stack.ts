f027486f99520ff9a98e90231a9848a81dad778c
allan.thekkepeedika <allan.thekkepeedika@gmail.com>
2025-05-07 18:20:44 +0000

    Infra: change in cognito

--- src/lib/auth/auth-stack.ts
+++ src/lib/auth/auth-stack.ts
@@ -1,33 +1,48 @@
 import * as cdk from "aws-cdk-lib";
-import { ManagedLoginVersion, UserPool, UserPoolClient, UserPoolDomain } from "aws-cdk-lib/aws-cognito";
-import { Construct } from "constructs"
+import {
+  ManagedLoginVersion,
+  OAuthScope,
+  UserPool,
+  UserPoolClient,
+  UserPoolClientIdentityProvider,
+  UserPoolDomain,
+} from "aws-cdk-lib/aws-cognito";
+import { Construct } from "constructs";
 
 export class AuthStack extends cdk.Stack {
-    public readonly userPool: UserPool
-    public readonly userPoolClient: UserPoolClient;
-    public readonly userPoolDomain: UserPoolDomain;
+  public readonly userPool: UserPool;
+  public readonly userPoolClient: UserPoolClient;
+  public readonly userPoolDomain: UserPoolDomain;
 
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
 
-        this.userPool = new UserPool(this, "UserPool", {
-            userPoolName: "CodeSenseiUserPool",
-            selfSignUpEnabled: true,
-            signInAliases: { email: true },
-            autoVerify: { email: true },
-            removalPolicy: cdk.RemovalPolicy.DESTROY
-        });
+    this.userPool = new UserPool(this, "UserPool", {
+      userPoolName: "CodeSenseiUserPool",
+      selfSignUpEnabled: true,
+      signInAliases: { email: true },
+      autoVerify: { email: true },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
-        this.userPoolClient = new UserPoolClient(this, "UserPoolClient", {
-            userPool: this.userPool,
-            generateSecret: false,
-            authFlows: { userPassword: true }
-        })
+    this.userPoolClient = new UserPoolClient(this, "UserPoolClient", {
+      userPool: this.userPool,
+      generateSecret: false,
+      authFlows: { userPassword: true },
+      disableOAuth: false,
+      oAuth: {
+        callbackUrls: ["http://localhost:5173/"],
+        logoutUrls: ["http://localhost:5173/"],
+        flows: { authorizationCodeGrant: true },
+        scopes: [OAuthScope.EMAIL, OAuthScope.OPENID, OAuthScope.PROFILE],
+      },
+      supportedIdentityProviders: [UserPoolClientIdentityProvider.COGNITO],
+    });
 
-        this.userPoolDomain = new UserPoolDomain(this, "UserPoolDomain", {
-            userPool: this.userPool,
-            cognitoDomain: { domainPrefix: "codesensei-app" },
-            managedLoginVersion: ManagedLoginVersion.NEWER_MANAGED_LOGIN
-        })
-    }
+    this.userPoolDomain = new UserPoolDomain(this, "UserPoolDomain", {
+      userPool: this.userPool,
+      cognitoDomain: { domainPrefix: "codesensei-app" },
+      managedLoginVersion: ManagedLoginVersion.NEWER_MANAGED_LOGIN,
+    });
+  }
 }
