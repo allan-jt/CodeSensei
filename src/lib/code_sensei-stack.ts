145ad0303d29eba4ea500a3a5171fcd54b7fbcd4
allan.thekkepeedika <allan.thekkepeedika@gmail.com>
2025-05-09 10:56:47 +0000

    AI ChatBot is deployable from main stack, but not yet integrated into api

--- src/lib/code_sensei-stack.ts
+++ src/lib/code_sensei-stack.ts
@@ -5,23 +5,25 @@ import { ExecutorStack } from "./executor/executor-stack";
 import { ApiGatewayStack } from "./api/api-gateway-stack";
 import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { AuthStack } from "./auth/auth-stack";
+import { InterviewAiCombinedStack } from "./chatbot/ai-interviewer-cdk-stack";
 
 export class CodeSenseiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const storage = new StorageStack(this, "StorageStack");
-    const executor = new ExecutorStack(this, "ExecutorStack", {
-      userTable: storage.userTable,
-      questionBankTable: storage.questionBankTable,
-      assessmentsTable: storage.assessmentsTable,
-      assessmentQuestionLocatorTable: storage.assessmentQuestionLocatorTable,
-    });
-    const auth = new AuthStack(this, "AuthStack", {
-      userTable: storage.userTable,
-    });
-    const api = new ApiGatewayStack(this, "APIGatewayStack", {
-      executionEntryLambda: executor.executorEntryLambda,
-    });
+    // const executor = new ExecutorStack(this, "ExecutorStack", {
+    //   userTable: storage.userTable,
+    //   questionBankTable: storage.questionBankTable,
+    //   assessmentsTable: storage.assessmentsTable,
+    //   assessmentQuestionLocatorTable: storage.assessmentQuestionLocatorTable,
+    // });
+    // const auth = new AuthStack(this, "AuthStack", {
+    //   userTable: storage.userTable,
+    // });
+    // const api = new ApiGatewayStack(this, "APIGatewayStack", {
+    //   executionEntryLambda: executor.executorEntryLambda,
+    // });
+    // const ai_interviewer = new InterviewAiCombinedStack(this, "AIChatBot");
   }
 }
