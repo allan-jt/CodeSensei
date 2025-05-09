cf99db785733e32c92265808e5870d1198c50a90
allan.thekkepeedika <allan.thekkepeedika@gmail.com>
2025-05-09 19:59:26 +0000

    Set up http api gateway and wired up metrics; need to adjust lambda code for cors

--- src/lib/code_sensei-stack.ts
+++ src/lib/code_sensei-stack.ts
@@ -6,35 +6,39 @@ import { ExecutorStack } from "./executor/executor-stack";
 import { ApiGatewayStack } from "./api/api-gateway-stack";
 import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { AuthStack } from "./auth/auth-stack";
-import { InterviewAiCombinedStack } from "./chatbot/ai-interviewer-cdk-stack";on
+import { InterviewAiCombinedStack } from "./chatbot/ai-interviewer-cdk-stack";
 
 export class CodeSenseiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const appName = "CodeSensei";
-    const storage = new StorageStack(this, "StorageStack", { stackName: `${appName}StorageStack` });
+
+    const storage = new StorageStack(this, "StorageStack");
+
     const metrics = new MetricsStack(this, "MetricsStack", {
       appName: appName,
       stackName: `${appName}MetricsStack`,
       metricsTable: storage.metricsTable,
-      assessmentsTable: storage.assessmentsTable
+      assessmentsTable: storage.assessmentsTable,
     });
-    const storage = new StorageStack(this, "StorageStack");
-    // const executor = new ExecutorStack(this, "ExecutorStack", {
-    //   userTable: storage.userTable,
-    //   questionBankTable: storage.questionBankTable,
-    //   assessmentsTable: storage.assessmentsTable,
-    //   assessmentQuestionLocatorTable: storage.assessmentQuestionLocatorTable,
-    // });
+
+    const executor = new ExecutorStack(this, "ExecutorStack", {
+      userTable: storage.userTable,
+      questionBankTable: storage.questionBankTable,
+      assessmentsTable: storage.assessmentsTable,
+      assessmentQuestionLocatorTable: storage.assessmentQuestionLocatorTable,
+    });
     const chatbot = new InterviewAiCombinedStack(this, "AIChatBot");
 
-    // const auth = new AuthStack(this, "AuthStack", {
-    //   userTable: storage.userTable,
-    // });
-    // const api = new ApiGatewayStack(this, "APIGatewayStack", {
-    //   executionEntryLambda: executor.executorEntryLambda,
-    //   chatbotEntryLambda: chatbot.chatbotEntryLambda,
-    // });
+    const auth = new AuthStack(this, "AuthStack", {
+      userTable: storage.userTable,
+    });
+    const api = new ApiGatewayStack(this, "APIGatewayStack", {
+      executionEntryLambda: executor.executorEntryLambda,
+      chatbotEntryLambda: chatbot.chatbotEntryLambda,
+      metricsDashboardLambda: metrics.metricsDashboardLambda,
+      metricsQuestionLambda: metrics.metricsQuestionLambda,
+    });
   }
 }
