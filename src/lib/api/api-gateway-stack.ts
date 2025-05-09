776fff022c6cc0a4e2f73d32c43740dc151cef25
allan.thekkepeedika <allan.thekkepeedika@gmail.com>
2025-05-09 19:59:26 +0000

    Set up http api gateway and wired up metrics; need to adjust lambda code for cors

--- src/lib/api/api-gateway-stack.ts
+++ src/lib/api/api-gateway-stack.ts
@@ -2,10 +2,13 @@ import * as cdk from "aws-cdk-lib";
 import { Function } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 import { SocketApiCustom } from "./custom-constructs/socket-api";
+import { HttpApiCustom } from "./custom-constructs/http-api";
 
 interface ApiGatewayProps extends cdk.StackProps {
   executionEntryLambda: Function;
   chatbotEntryLambda: Function;
+  metricsDashboardLambda: Function;
+  metricsQuestionLambda: Function;
 }
 
 export class ApiGatewayStack extends cdk.Stack {
@@ -14,7 +17,12 @@ export class ApiGatewayStack extends cdk.Stack {
 
     const socketApi = new SocketApiCustom(this, "SocketAPI", {
       executionEntryLambda: props.executionEntryLambda,
-      chatbotEntryLambda: props.chatbotEntryLambda
+      chatbotEntryLambda: props.chatbotEntryLambda,
+    });
+
+    const httpApi = new HttpApiCustom(this, "HttpApi", {
+      metricsDashboardLambda: props.metricsDashboardLambda,
+      metricsQuestionLambda: props.metricsQuestionLambda,
     });
   }
 }
